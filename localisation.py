8b9ff80d2aef7b712192a25497f4910313aaa928
David <danstee1@my.bcit.ca>
2024-11-23 13:38:29 -0800

    Add main() to localisation module

--- localisation.py
+++ localisation.py
@@ -6,6 +6,7 @@ import json
 
 LOCALISATION_FILE = "localisation.json"
 
+
 def get_text(namespace: str, loc_id: str) -> str:
     try:
         with open(LOCALISATION_FILE, 'r') as text_file:
@@ -16,3 +17,11 @@ def get_text(namespace: str, loc_id: str) -> str:
     except KeyError:
         return "LOCALISATION NOT FOUND"
 
+
+def main():
+    example_text = get_text("intro", "0000")
+    print(example_text)
+
+
+if __name__ == "__main__":
+    main()

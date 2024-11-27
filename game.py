277841ebcb023311e717373b95aac9384355f173
David <danstee1@my.bcit.ca>
2024-11-27 15:00:45 -0800

    Add stat selection to make_character

--- game.py
+++ game.py
@@ -48,6 +48,37 @@ def make_character() -> dict:
     """
     character = {"x_coord": 0, "y_coord": 0, "hp": 5, "level": 1, "visited_rooms": [(0, 0)],
                  "insight": 0, "might": 0, "cunning": 0}
+    primary_prompt = (f"Pick your primary stat:"
+                      f"\n1. Insight - your intelligence and ability to notice things"
+                      f"\n2. Might - your physical strength, and endurance"
+                      f"\n3. Cunning - your deceptiveness and stealth")
+    secondary_prompt = (f"\nPick your secondary stat:"
+                        f"\n1. Insight - your intelligence and ability to notice things"
+                        f"\n2. Might - your physical strength, and endurance"
+                        f"\n3. Cunning - your deceptiveness and stealth")
+    primary_stat = input(primary_prompt)
+    while not primary_stat.isnumeric() or not (0 < int(primary_stat) < 4):
+        print("\nInvalid input. Please enter a number from 1-3.")
+        primary_stat = input(primary_prompt)
+    primary_stat = int(primary_stat)
+
+    secondary_stat = input(secondary_prompt)
+
+    while not secondary_stat.isnumeric() or not (0 < int(secondary_stat) < 4):
+        print("\nInvalid input. Please enter a number from 1-3.")
+        secondary_stat = input(secondary_prompt)
+    secondary_stat = int(secondary_stat)
+
+    number_to_stat = {1: "insight", 2: "might", 3: "cunning"}
+    primary_stat = number_to_stat[primary_stat]
+    secondary_stat = number_to_stat[secondary_stat]
+    character[primary_stat] = 2
+    character[secondary_stat] = 1
+    print(f"Your stats are:"
+          f"\nInsight: {character["insight"]}"
+          f"\nMight: {character["might"]}"
+          f"\nCunning: {character["cunning"]}")
+
     return character
 
 
@@ -58,11 +89,11 @@ def level_up_character(game_state: dict):
     new_stats = {"insight": 0, "might": 0, "cunning": 0}
     for num in enumerate(stat_progression):
         if character["insight"] == num[1] and num[1] < len(stat_progression):
-            new_stats["insight"] = stat_progression[num[0]+1]
+            new_stats["insight"] = stat_progression[num[0] + 1]
         if character["might"] == num[1] and num[1] < len(stat_progression):
-            new_stats["might"] = stat_progression[num[0]+1]
+            new_stats["might"] = stat_progression[num[0] + 1]
         if character["cunning"] == num[1] and num[0] < len(stat_progression):
-            new_stats["cunning"] = stat_progression[num[0]+1]
+            new_stats["cunning"] = stat_progression[num[0] + 1]
     character["insight"] = new_stats["insight"]
     character["might"] = new_stats["might"]
     character["cunning"] = new_stats["cunning"]

d10c5209a5e5eeb814c14ec02ef50c81bf91b053
David <danstee1@my.bcit.ca>
2024-11-29 15:46:07 -0800

    Decompose character creation

--- game.py
+++ game.py
@@ -40,6 +40,20 @@ def make_board(rows: int, columns: int) -> dict:
     return new_board
 
 
+def get_stat_selection(prompt: str, valid_stats: list[str]) -> str:
+    for number, stat in enumerate(valid_stats, 1):
+        prompt += f"\n{number}. {localisation.get_text("stats", stat)}"
+    num_selection = input(prompt)
+
+    while not num_selection.isnumeric() or not (1 <= int(num_selection) <= len(valid_stats)):
+        print(f"\nInvalid input. Please enter 1 - {len(valid_stats)}.")
+        num_selection = input(prompt)
+
+    stat = valid_stats[int(num_selection)-1]
+    valid_stats.remove(stat)
+    return stat
+
+
 def make_character() -> dict:
     """
     Make the player character.
@@ -51,28 +65,12 @@ def make_character() -> dict:
     """
     character = {"x_coord": 0, "y_coord": 0, "hp": 5, "level": 1, "visited_rooms": [],
                  "insight": 0, "might": 0, "cunning": 0}
+
     stats = ["insight", "might", "cunning"]
-
-    primary_prompt = f"{localisation.get_text("prompt", "first_stat", True)}"
-    for number, stat in enumerate(stats, 1):
-        primary_prompt += f"\n{number}. {localisation.get_text("stats", stat)}"
-
-    primary_stat = input(primary_prompt)
-    while not primary_stat.isnumeric() or not int(primary_stat) in [1, 2, 3]:
-        print("\nInvalid input. Please enter 1 - 3.")
-        primary_stat = input(primary_prompt)
-    primary_stat = stats[int(primary_stat)-1]
-    stats.remove(primary_stat)
-
-    secondary_prompt = f"{localisation.get_text("prompt", "second_stat", True)}"
-    for number, stat in enumerate(stats, 1):
-        secondary_prompt += f"\n{number}. {localisation.get_text("stats", stat)}"
-
-    secondary_stat = input(secondary_prompt)
-    while not secondary_stat.isnumeric() or not int(secondary_stat) in [1, 2]:
-        print("\nInvalid input. Please enter 1 or 2.")
-        secondary_stat = input(secondary_prompt)
-    secondary_stat = stats[int(secondary_stat)-1]
+    primary_stat = get_stat_selection(localisation.get_text("prompt", "first_stat", True),
+                                      stats)
+    secondary_stat = get_stat_selection(localisation.get_text("prompt", "second_stat", True),
+                                        stats)
 
     character[primary_stat] = 2
     character[secondary_stat] = 1

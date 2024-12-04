25438530a03182e7e14024da9ad48ffb59183a2f
David <danstee1@my.bcit.ca>
2024-12-03 19:50:13 -0800

    Add unit tests for roll_for_encounter

--- unit_tests/encounter/test_roll_for_encounter.py
+++ unit_tests/encounter/test_roll_for_encounter.py
@@ -1,6 +1,36 @@
 from unittest import TestCase
+from unittest.mock import patch
+
+from encounter import roll_for_encounter
 
 
 class Test(TestCase):
-    def test_roll_for_encounter(self):
-        self.fail()
+    @patch('random.randint', side_effect=[1])
+    def test_roll_for_encounter_success(self, _):
+        self.assertTrue(roll_for_encounter())
+
+    @patch('random.randint', side_effect=[10])
+    def test_roll_for_encounter_narrow_success(self, _):
+        self.assertTrue(roll_for_encounter())
+
+    @patch('random.randint', side_effect=[100])
+    def test_roll_for_encounter_failure(self, _):
+        self.assertFalse(roll_for_encounter())
+
+    @patch('random.randint', side_effect=[11])
+    def test_roll_for_encounter_narrow_failure(self, _):
+        self.assertFalse(roll_for_encounter())
+
+    @patch('random.randint', side_effect=[40])
+    def test_roll_for_encounter_success_with_different_rate(self, _):
+        self.assertTrue(roll_for_encounter(encounter_rate=60))
+
+    @patch('random.randint', side_effect=[80])
+    def test_roll_for_encounter_failure_with_different_rate(self, _):
+        self.assertFalse(roll_for_encounter(encounter_rate=60))
+
+    def test_roll_for_encounter_guaranteed_success(self):
+        self.assertTrue(roll_for_encounter(encounter_rate=100))
+
+    def test_roll_for_encounter_guaranteed_failure(self):
+        self.assertFalse(roll_for_encounter(encounter_rate=0))

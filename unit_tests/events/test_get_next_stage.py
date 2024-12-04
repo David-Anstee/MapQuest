53a3bdebf6e406339bf9abfb11c589e6e9c3c31a
David <danstee1@my.bcit.ca>
2024-12-03 20:18:00 -0800

    Add unit tests for get_next_stage

--- unit_tests/events/test_get_next_stage.py
+++ unit_tests/events/test_get_next_stage.py
@@ -1,6 +1,28 @@
 from unittest import TestCase
-
+from unittest.mock import patch
+from events import get_next_stage
 
 class Test(TestCase):
-    def test_get_next_stage(self):
-        self.fail()
+
+    @patch('game.skill_check', side_effect=[True])
+    def test_get_next_stage_skill_check_success(self, _):
+        game_state = {}
+        choice = {"skill_check": ["insight", 6], "success": "1", "failure": "2"}
+        expected = "1"
+        actual = get_next_stage(game_state=game_state, choice=choice)
+        self.assertEqual(expected, actual)
+
+    @patch('game.skill_check', side_effect=[False])
+    def test_get_next_stage_skill_check_failure(self, _):
+        game_state = {}
+        choice = {"skill_check": ["insight", 6], "success": "1", "failure": "2"}
+        expected = "2"
+        actual = get_next_stage(game_state=game_state, choice=choice)
+        self.assertEqual(expected, actual)
+
+    def test_get_next_stage_skill_no_skill_check(self):
+        game_state = {}
+        choice = {"success": "1", "failure": "2"}
+        expected = "1"
+        actual = get_next_stage(game_state=game_state, choice=choice)
+        self.assertEqual(expected, actual)

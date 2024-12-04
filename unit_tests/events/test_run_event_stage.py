0c82edd51cea3e04f44da743fa08e57299457fed
David <danstee1@my.bcit.ca>
2024-12-03 20:28:03 -0800

    Add unit test for run_event_stage

--- unit_tests/events/test_run_event_stage.py
+++ unit_tests/events/test_run_event_stage.py
@@ -1,6 +1,13 @@
 from unittest import TestCase
+from unittest.mock import patch
+from events import run_event_stage
 
 
 class Test(TestCase):
-    def test_run_event_stage(self):
-        self.fail()
+    @patch('events.apply_effects')
+    @patch('player_input.get_user_input', side_effect=["choice_a"])
+    @patch("events.get_next_stage", side_effect=["1"])
+    def test_run_event_stage_returns_correct_stage(self, _, __, ___):
+        expected = "1"
+        actual = run_event_stage({}, {"description": "", "options": {"choice_a": "1"}})
+        self.assertEqual(expected, actual)

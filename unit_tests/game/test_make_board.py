69932e847cbd6abe3b1ce016945ccd6409419a5d
David <danstee1@my.bcit.ca>
2024-12-03 21:22:23 -0800

    Create unit test for make_board

--- unit_tests/game/test_make_board.py
+++ unit_tests/game/test_make_board.py
@@ -1,6 +1,23 @@
 from unittest import TestCase
+from unittest.mock import patch
+
+import tile
+from game import make_board
 
 
 class Test(TestCase):
-    def test_make_board(self):
-        self.fail()
+    @patch("data.get_data", side_effect=[{}])
+    @patch("tile.make_tile", side_effect=["meadow"])
+    def test_make_board_dimensions_lower_boundary(self, _, __):
+        board = make_board(1, 1)
+        expected = 1
+        actual = len(board)
+        self.assertEqual(expected, actual)
+
+    @patch("data.get_data", side_effect=[{}])
+    @patch("tile.make_tile", side_effect=["meadow" for num in range(0, 25)])
+    def test_make_board_dimensions_average_size(self, _, mock_response):
+        board = make_board(5, 5)
+        expected = 25
+        actual = len(board)
+        self.assertEqual(expected, actual)

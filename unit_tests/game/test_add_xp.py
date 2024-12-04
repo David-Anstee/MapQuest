a5937face11d5926b7629ed6a1ad49fcfe483d67
David <danstee1@my.bcit.ca>
2024-12-03 20:55:26 -0800

    Create unit test for add_xp

--- unit_tests/game/test_add_xp.py
+++ unit_tests/game/test_add_xp.py
@@ -1,6 +1,21 @@
+import io
 from unittest import TestCase
+from unittest.mock import patch
+from game import add_xp
 
 
 class Test(TestCase):
-    def test_add_xp(self):
-        self.fail()
+    def test_add_xp_correct_amount(self):
+        game_state = {"character": {"xp": 0, "level": 1}}
+        add_xp(game_state=game_state, amount=1)
+        expected = 1
+        actual = game_state["character"]["xp"]
+        self.assertEqual(expected, actual)
+
+    @patch("game.level_up_character")
+    def test_add_xp_reduces_xp_on_level_up(self, _):
+        game_state = {"character": {"xp": 0, "level": 1}}
+        add_xp(game_state=game_state, amount=2)
+        expected = 0
+        actual = game_state["character"]["xp"]
+        self.assertEqual(expected, actual)

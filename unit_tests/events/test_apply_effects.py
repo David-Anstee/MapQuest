19fcca1550e6e0a4da33dd32ca5b55a63c6dbdca
David <danstee1@my.bcit.ca>
2024-12-03 20:02:05 -0800

    Add unit tests for apply_effects

--- unit_tests/events/test_apply_effects.py
+++ unit_tests/events/test_apply_effects.py
@@ -1,6 +1,28 @@
 from unittest import TestCase
+from events import apply_effects
 
 
 class Test(TestCase):
-    def test_apply_effects(self):
-        self.fail()
+    def test_apply_effects_no_effects(self):
+        game_state = {"character": {"hp": 5}}
+        event_stage = {}
+        apply_effects(game_state=game_state, event_stage=event_stage)
+        expected = 5
+        actual = game_state["character"]["hp"]
+        self.assertEqual(expected, actual)
+
+    def test_apply_effects_one_effect(self):
+        game_state = {"character": {"hp": 5}}
+        event_stage = {"effects": {"character": {"hp": -1}}}
+        apply_effects(game_state=game_state, event_stage=event_stage)
+        expected = 4
+        actual = game_state["character"]["hp"]
+        self.assertEqual(expected, actual)
+
+    def test_apply_effects_multiple_effects(self):
+        game_state = {"character": {"hp": 5, "xp": 3}}
+        event_stage = {"effects": {"character": {"hp": -1, "xp": -1}}}
+        apply_effects(game_state=game_state, event_stage=event_stage)
+        expected = {"hp": 4, "xp": 2}
+        actual = game_state["character"]
+        self.assertDictEqual(expected, actual)

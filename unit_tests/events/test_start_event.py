6b3d4a94273b8afdad2e8534afefe229768c891e
David <danstee1@my.bcit.ca>
2024-12-03 20:41:50 -0800

    Create unit test for start_event

--- unit_tests/events/test_start_event.py
+++ unit_tests/events/test_start_event.py
@@ -1,6 +1,15 @@
 from unittest import TestCase
+from unittest.mock import patch
+
+import events
+from events import start_event
 
 
 class Test(TestCase):
-    def test_start_event(self):
-        self.fail()
+    @patch("events.get_event_sequence", side_effect=[{"0": {"xp": 4}}])
+    @patch("events.run_event_stage", side_effect=["-1"])
+    @patch('game.add_xp')
+    def test_start_event_exits_gracefully(self, _, __, ___):
+        game_state = {}
+        tile_id = "AA"
+        events.start_event(game_state=game_state, tile_id=tile_id)
